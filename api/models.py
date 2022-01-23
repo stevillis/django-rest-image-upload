2e3e1e5212597fb9412ec6e0b1961a935867f50e
Stévillis Sousa <stevillis@hotmail.com>
2022-01-23 11:17:40 -0400

    Increase descricao length

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Imagem(models.Model):
-    descricao = models.CharField(max_length=30)
+    descricao = models.CharField(max_length=100)
     imagem = models.ImageField()
     data = models.DateTimeField(auto_now_add=True)
 
