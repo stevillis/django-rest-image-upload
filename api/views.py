daa79670a26d57dc0edf67ebf533b59bbde9a8fb
stevillis <stevillis@hotmail.com>
2022-01-23 02:06:12 -0400

    Add function to detect cat faces

--- api/views.py
+++ api/views.py
@@ -1,8 +1,27 @@
+import cv2
 from django.http import JsonResponse
 from rest_framework import viewsets
 
 from api.models import Imagem
 from api.serializers import ImagemSerializer
+
+
+def find_cats_on_image(foto):
+    # load the input image and convert it to grayscale
+    image = cv2.imread(foto)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # load the cat detector Haar cascade, then detect cat faces
+    # in the input image
+    detector = cv2.CascadeClassifier('haarcascade_frontalcatface.xml')
+    rects = detector.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=10, minSize=(75, 75))
+
+    # loop over the cat faces and draw a rectangle surrounding each
+    for (i, (x, y, w, h)) in enumerate(rects):
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.putText(image, "Gato {}".format(i + 1), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255, 2))
+
+    return image
 
 
 class ImagemViewSet(viewsets.ModelViewSet):
@@ -14,8 +33,11 @@ class ImagemViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
 
+        foto = serializer.data.get("foto", "")
+        foto_analisada = find_cats_on_image(foto)
+
         data = {
-            "foto": serializer.data.get("foto", ""),
+            "foto": foto_analisada,
             "descricao": serializer.data.get("descricao", ""),
         }
         return JsonResponse(data)
