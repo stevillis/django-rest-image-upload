b3781673ace35f235261232aaa6d735f6b633724
Stévillis Sousa <stevillis@hotmail.com>
2022-01-23 01:25:02 -0400

    Upload file and show it from server

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from rest_framework import viewsets
 
 from api.models import Imagem
@@ -7,3 +8,14 @@ from api.serializers import ImagemSerializer
 class ImagemViewSet(viewsets.ModelViewSet):
     queryset = Imagem.objects.all()
     serializer_class = ImagemSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        data = {
+            "foto": serializer.data.get("foto", ""),
+            "descricao": serializer.data.get("descricao", ""),
+        }
+        return JsonResponse(data)
--- app/static/app/js/script.js
+++ app/static/app/js/script.js
@@ -1,6 +1,40 @@
+const getUrl = window.location;
+const baseUrl = getUrl.protocol + "//" + getUrl.host;
+
 const inputFoto = document.querySelector("#foto");
-const inputDescricao = document.querySelector("#descricao");
-inputFoto.addEventListener("change", (event) => {
-  const file = event.target.files[0];
-  inputDescricao.value = file.name;
+const inputsubmit = document.querySelector("#submit");
+const imageFromServer = document.querySelector("#imageFromServer");
+const small = document.querySelector("small");
+
+function validateForm() {
+  const file = inputFoto.files[0];
+  const descricao = file.name;
+
+  if (file) {
+    let data = new FormData();
+    data.append("foto", file);
+    data.append("descricao", descricao);
+
+    fetch(`${baseUrl}/imagens/`, {
+      method: "POST",
+      body: data,
+    }).then((response) => {
+      if (response.status === 200) {
+        response.json().then((data) => {
+          imageFromServer.style.display = "block";
+          imageFromServer.setAttribute("src", data.foto);
+          imageFromServer.setAttribute("descricao", data.descricao);
+        });
+      }
+    });
+  } else {
+    small.style.display = "block";
+  }
+}
+
+inputsubmit.addEventListener("click", () => {
+  validateForm();
 });
+
+small.style.display = "none";
+imageFromServer.style.display = "none";
